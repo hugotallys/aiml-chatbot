84f953164d5231d8c9f7c2259ee98895359f0fbd
hugotallys <htmo@ic.ufal.br>
2021-04-01 21:14:00 -0300

    update files

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
 import aiml
+import string
 
 from unidecode import unidecode
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.static_folder = 'static'
+app.static_folder = "static"
+
+def clean_input(text):
+    text = unidecode(text)
+    return text.translate(str.maketrans("", "", string.punctuation))
 
 @app.route("/")
 def home():
@@ -13,20 +18,17 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    
-    userText = unidecode(request.args.get('msg'))
-
-    bot_response = kernel.respond(userText)
-
-    return bot_response
+    userText = clean_input(request.args.get("msg"))
+    return kernel.respond(userText)
 
 if __name__ == "__main__":
 
     kernel = aiml.Kernel()
 
+    load_brain = False
     brain_file = "brains/turista_bot_brain.brn"
 
-    if os.path.exists(brain_file):
+    if os.path.exists(brain_file) and load_brain:
         kernel.bootstrap(brainFile=brain_file)
     else:
         kernel.bootstrap(learnFiles="turista-bot-startup.xml", commands="load turista bot")
